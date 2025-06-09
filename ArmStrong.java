de389684a1ab5a7cf1d7023ef98c184599d81154
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-06-09 11:24:34 +0530

    using while loop

---
+++ ArmStrong.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ArmStrong {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp =n;
+        int rev =0;
+
+        while(n>0){
+            int digit = n %10;
+            rev = digit*digit*digit+rev;
+            n = n/10;
+        }
+        if(temp==rev){
+            System.out.println(temp + " is a ArmStrong Number");
+
+        }
+        else{
+            System.out.println( temp + " It is not a ArmStrong Number");
+        }
+
+
+
+    }
+    
+}
