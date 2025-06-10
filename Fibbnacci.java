a30fbe5a70d0491acaa6363088b88851cf94d4f1
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-06-10 10:44:39 +0530

    Fibnacci series

---
+++ Fibbnacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Fibbnacci {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int N = sc.nextInt();
+        int start=0;
+        int end=1;
+
+        for(int i=0;i<N;i++){
+            int fib = start+end;
+            System.out.print(start+" ");
+            start=end;
+            end= fib;
+
+        }
+
+
+    }
+    
+}
