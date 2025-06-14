f0da446b54cb42b4e38ca07459b4ad63ab4f34f5
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-06-14 10:48:46 +0530

    Print grade based Marks

---
+++ GradeMark.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class GradeMark {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int marks1 = sc.nextInt();
+        int marks2 = sc.nextInt();
+        int marks3 = sc.nextInt(); 
+
+        System.out.println(gradeCheck(marks1));
+        System.out.println(gradeCheck(marks2));
+        System.out.println(gradeCheck(marks3));
+
+    }
+    public static String gradeCheck(int marks){
+        if(marks >90){
+            return "A";
+        }
+        else if(marks >70){
+            return "B";
+        }
+        else if(marks >=40){
+           return "C";
+        }
+        else{
+            return "Fail";
+        }
+    }
+
+    
+}
