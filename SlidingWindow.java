e2cdf3f9a9ac5815f3e0e7ed22d6c4a7df3d2cbd
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-06-29 10:18:30 +0530

    (n*K)

---
+++ SlidingWindow.java
@@ -0,0 +1,27 @@
+public class SlidingWindow {
+    public static void main(String[] args){
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        System.out.print("Maximum in each window: ");
+        slidingWin(arr, k);
+      
+
+    }
+    public static void slidingWin(int[] arr,int k){
+        int n = arr.length;
+        if(n<k || k<=0){
+            System.out.println("Invalid output");
+        }
+        for(int i =0;i<=n-k;i++){
+            int max = arr[i];
+            for(int j =i+1;j<i+k;j++){
+                if(arr[j] > max){
+                    max = arr[j];
+                }
+            }
+            System.out.print(max+" ");
+        }
+    }
+
+    
+}
